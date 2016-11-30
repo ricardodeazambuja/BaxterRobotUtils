ad43db1b61cf66cb4e4e627670d162cab5c8b7fe
Ricardo de Azambuja <ricardo.azambuja@gmail.com>
2016-11-30 14:20:03 +0000

    Access the ik_service from BAXTER robot
    File to be used during the APRIL Project workshop - 2016

---
+++ ik_client.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+"""
+Inverse Kinematics
+"""
+import argparse
+import struct
+import sys
+import baxter_interface
+import rospy
+
+import time
+
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+from std_msgs.msg import Header
+
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest,
+)
+
+
+
+class ik_service(object):
+    def __init__(self, limb, speed=0.3):
+        rospy.init_node("ik_service", anonymous=True)
+        self.ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
+        self.iksvc = rospy.ServiceProxy(self.ns, SolvePositionIK)
+
+        # Starts the Libm interface
+        self.arm = baxter_interface.Limb(limb)
+        self.arm.set_command_timeout(2.0)
+        self.arm.set_joint_position_speed(speed)
+
+
+    def ik_call(self, xyz, quat):
+        self.ikreq = SolvePositionIKRequest()
+        hdr = Header(stamp=rospy.Time.now(), frame_id='base')
+        pose = PoseStamped(
+                header=hdr,
+                pose=Pose(
+                    position=Point(
+                        x=xyz[0],
+                        y=xyz[1],
+                        z=xyz[2],
+                    ),
+                    orientation=Quaternion(
+                        x=quat[0],
+                        y=quat[1],
+                        z=quat[2],
+                        w=quat[3],
+                    ),
+                ),
+            )
+        self.ikreq.pose_stamp.append(pose)
+
+        try:
+            rospy.wait_for_service(self.ns, 5.0) # blocks until a service is available
+            resp = self.iksvc(self.ikreq)
+
+        except Exception as err:
+            print err
+            return 1
+
+        # Check if result valid
+        resp_seeds = struct.unpack('<%dB' % len(resp.result_type), resp.result_type)
+        if (resp_seeds[0] != resp.RESULT_INVALID):
+            # Format solution into Limb API-compatible dictionary
+            self.joint_names = resp.joints[0].name
+            self.joint_values = resp.joints[0].position
+            self.limb_joints = dict(zip(self.joint_names, self.joint_values))
+        else:
+            print "Invalid IK"
+            return 2
+
+        return 0
+
+
+    def ik_move(self):
+        self.arm.set_joint_positions(self.limb_joints)
